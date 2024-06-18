7e2cbb59a7355e62c895320cb74bb5680d224fe3
Dennis Boachie Boateng <dennisboachie9@gmail.com>
2024-06-18 19:05:52 +0000

    Removed header and socials

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,4 +1,3 @@
-import Footer from "@/components/Footer";
 import {Outlet} from "react-router-dom";
 import {ModeToggle} from "@/components/mode-toggle.tsx";
 
@@ -7,7 +6,6 @@ const Layout = () => {
         <div>
             <ModeToggle className='absolute top-2 right-14 z-[10000000000]'/>
             <Outlet/>
-            <Footer/>
         </div>
     );
 };
