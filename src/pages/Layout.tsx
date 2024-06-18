f24db18d40f55a77ba0bc82df761247b0720cd3f
Dennis Boachie Boateng <dennisboachie9@gmail.com>
2024-06-18 19:02:37 +0000

    Removed header and socials

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,11 +1,11 @@
 import Footer from "@/components/Footer";
 import {Outlet} from "react-router-dom";
-import NavbarFloat from "@/components/Navbar/NavbarFloat";
+import {ModeToggle} from "@/components/mode-toggle.tsx";
 
 const Layout = () => {
     return (
         <div>
-            <NavbarFloat/>
+            <ModeToggle className='absolute top-2 right-14 z-[10000000000]'/>
             <Outlet/>
             <Footer/>
         </div>
